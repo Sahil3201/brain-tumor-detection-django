0867a2256146b67e69f4ff7fba190c29436706e1
Sahil <lunawatsahil321@gmail.com>
2022-10-21 13:33:36 +0530

    Added image classifier

--- cv/urls.py
+++ cv/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('predict/', views.getPredictions, name='prediction'),
     path('predict-new/', views.getPredictionsNew, name='predictionNew'),
+    path('predict-new-with-img-classifier/', views.getPredictionsNewWithImgClassifier, 
+        name='predictionNewWithImgClassifier'),
     path('history/', views.BrainScansListView.as_view(), name='history'),
 ]
--- cv/views.py
+++ cv/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from .model import ModelDeployment
 from .forms import *
 from .models import BrainScans
+from . import imageClassifier
 
 def index(request):
     return HttpResponse("<h1>Hello, world. You're at the cv index.</h1><h3>Try to use paths:<br>- /predict</h3>")
@@ -34,6 +35,8 @@ def getPredictions(request):
 
 def getPredictionsNew(request):
     last_prediction = BrainScans.objects.last()
+    if last_prediction and last_prediction.imageName:
+        last_prediction.imageName = last_prediction.imageName[:20]
 
     if request.method == 'POST':
         form = BrainScansFormNew(request.POST, request.FILES)
@@ -67,4 +70,43 @@ class BrainScansListView(ListView):
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
+    #     context['predict_page'] = timezone.now()
     #     return context
+
+def getPredictionsNewWithImgClassifier(request):
+    last_prediction = BrainScans.objects.last()
+    if last_prediction and last_prediction.imageName:
+        last_prediction.imageName = last_prediction.imageName[:20]
+
+    if request.method == 'POST':
+        form = BrainScansFormNew(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            # filename = form.instance.image.file.name
+
+            isMri = imageClassifier.get_class_of_image(form.instance.image.file.name)
+            print("isMri!!!!!!!!!!!!!!!!!!!")
+            print(isMri)
+            prediction, imageName, original_image = ModelDeployment.get_prediction_from_image_upload_new(form.instance.image.file.name)
+            form.instance.prediction = round(prediction*100,2)
+            form.instance.imageName = str(request.FILES['image']) #filename
+            if isMri:
+                form.save()
+            else:
+                form.instance.delete()
+
+            form = BrainScansFormNew()
+            return render(request, 'brain_scan_predict-new-with-MRI-classifier.html',
+            {'form' : form,
+            "prediction": round(prediction*100,2),
+            "imageName": str(request.FILES['image']), # str(filename),
+            'original_image': original_image.decode('utf-8'),
+            'isNotMri': not isMri,
+            "last_prediction": last_prediction}
+            )
+    else:
+        form = BrainScansFormNew()
+        return render(request, 'brain_scan_predict-new-with-MRI-classifier.html',
+        {'form' : form,
+            "last_prediction": last_prediction})
